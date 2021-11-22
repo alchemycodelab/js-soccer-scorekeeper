57cc4eabb388ecb5f868734383a39fc8e7b59174
Danny Cairns <dpcairns@gmail.com>
2021-11-21 20:53:20 -0800

    change location

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+import { renderTeam, renderGame } from './render-utils.js';
 const currentGameEl = document.getElementById('current-game-container');
 const pastGamesEl = document.getElementById('past-games-container');
 
@@ -75,35 +76,6 @@ function displayCurrentGameEl() {
     gameEl.classList.add('current');
 
     currentGameEl.append(gameEl);
-}
-
-function renderGame(game) {
-    const div = document.createElement('div');
-    const team1Div = renderTeam(game.name1, game.score1);
-    const team2Div = renderTeam(game.name2, game.score2);
-
-    div.append(team1Div, team2Div);
-
-    div.classList.add('game');
-
-    return div;
-}
-
-function renderTeam(name, score) {
-    const teamDiv = document.createElement('div');
-    const nameDiv = document.createElement('p');
-    const scoreDiv = document.createElement('p');
-
-    teamDiv.classList.add('team');
-    nameDiv.classList.add('name');
-    scoreDiv.classList.add('score');
-
-    nameDiv.textContent = name;
-    scoreDiv.textContent = score;
-
-    teamDiv.append(nameDiv, scoreDiv);
-
-    return teamDiv;
 }
 
 
